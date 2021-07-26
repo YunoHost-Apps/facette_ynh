731120877dca36995c9e363a5d64f51cf2862d09
ericgaspar <junk.eg@free.fr>
2021-07-26 23:31:43 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="rrdtool rrdcached collectd"
+#pkg_dependencies="rrdtool rrdcached collectd"
 
 #=================================================
 # PERSONAL HELPERS
